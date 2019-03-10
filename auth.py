19840ba9af4bc0f5451d5587bc3decdfb3e6e080
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2019-03-10 12:47:14 +0000

    Added more comments

--- auth.py
+++ auth.py
@@ -3,15 +3,22 @@ from __future__ import print_function
 import random
 import praw
 
+
 # 1. Add user to the CaptainScarletBots app and change the first input below
-r = praw.Reddit('CAPTAINBLACK',user_agent="CaptainScarletBots")
+r = praw.Reddit(input("Who do you want to authorise? "),user_agent="CaptainScarletBots")
 
 # 2. Run this and visit url. You may need to remove +s from url
 state = str(random.randint(0, 65000))
-url = r.auth.url("identity submit edit", state, 'permanent')
+scope = "account creddits edit flair history identity livemanage modconfig modcontributors modflair modlog modothers modposts modself modwiki mysubreddits privatemessages read report save submit subscribe vote wikiedit wikiread"
+url = r.auth.url(scope, state, 'permanent')
+url = url.replace("+".join(i for i in scope.replace(" ","+")),
+                  scope.replace(" ","+")
+                 )
+print("Go to this URL to get token")
 print(url)
 
 # 3. Run this with code taken from teh result of step 2.
-r.auth.authorize(code="???")
+code = r.auth.authorize(code=input("Paste the token here: "))
 
 # 4. Save the output in praw.ini as refresh_token
+print(code)
--- bot.py
+++ bot.py
@@ -2,11 +2,23 @@ from __future__ import print_function
 import random
 import praw
 
-r = praw.Reddit('CAPTAINBLACK',user_agent="CAPTAINBLACK")
+options = [
+           ("CAPTAINBLACK","This is Captain Black, relaying instructions from the Mysterons on Mars. You know what you must do."),
+           ("MYSTERONS","This is the voice of the Mysterons. We know you can hear us, Earthmen. We will be avenged."),
+           ("NARRATOR","The Mysterons, sworn enemies of Earth, possessing the ability to recreate an exact likeness of an object or person... but first they must destroy.\n\n"
+                       "[cat howls, gun fires]\n\n"
+                       "Leading the fight, one man fate has made indestructible. His name... {{USER}}.")
+          ]
+
+user, comment = random.choice(options)
+
+r = praw.Reddit(user,user_agent="CaptainScarletBots")
 
 subreddit = r.subreddit("CaptainScarletMemes")
-print(subreddit.display_name)
 
-print(r.user.me())
-
-
+for i in subreddit.new():
+    if len(i.comments.list()) == 0:
+        comment = comment.replace("{{USER}}","/u/"+i.author.name)
+        print(r.user.me(), comment)
+        i.reply(comment)
+        break
