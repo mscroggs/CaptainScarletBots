a9dc513da1ef35f6c882f1262807de2951a4a7f3
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2019-03-11 07:58:04 +0000

    Added more comments, onyl check last 20

--- bot.py
+++ bot.py
@@ -9,6 +9,21 @@ options = [
            ("NARRATOR",["The Mysterons, sworn enemies of Earth, possessing the ability to recreate an exact likeness of an object or person... but first they must destroy.\n\n"
                        "[cat howls, gun fires]\n\n"
                        "Leading the fight, one man fate has made indestructible. His name... {{USER}}."]),
+           ("NARRATOR",["{{USER}} ({{USER}}). "
+                        "He's the one who knows the Mysteron Game, "
+                        "and things that they plan.",
+                        "{{USER}} ({{USER}}). "
+                        "To his Martian foes a dangerous name, "
+                        "a superman.",
+                        "They crash {{USER}}, and his body may burn. "
+                        "They smash {{USER}}, but they know he'll return, "
+                        "to live again.",
+                        "{{USER}} ({{USER}}). "
+                        "As the Angels are flying wing to wing, "
+                        "into the scene, Spectrum Is Green.",
+                        "{{USER}} ({{USER}}). "
+                        "Though the Mysteron's plan to conquer the earth, "
+                        "this indestructible man will show what he's worth."]),
            ("GREEN",["Angels one, two and three. Immediate launch."]),
            ("WHITE",["This is an operational base, not a rest centre!",
                      "What's the meaning of this, {{USER}}? Bursting in here and making demands?"]),
@@ -20,7 +35,10 @@ options = [
                        "Don't worry about that.",
                        "No, sir. Nothing has come within a hundred miles of this base since the Red Alert, except {{USER}} of course."]),
            ("MACEY",["Thirteen... THIRTEEN!?!",
-                     "I can't hold it, I just can't hold it! It's out of my control! It's out of my control!"])
+                     "I can't hold it, I just can't hold it! It's out of my control! It's out of my control!"]),
+           ("PS2",["Yet again your skills are very impressive. Well done {{USER}}.",
+                   "Time and time again you have proven yourself to be the greatest asset Spectrum has against the Mysteron threat. You are truly a hero of our times {{USER}}. I owe you my life.\n\nNow take a well earned rest!",
+                   "You are a credit to Spectrum and the world. I owe you my gratitute.\n\nWell done."])
           ]
 
 user, comments = random.choice(options)
@@ -30,7 +48,7 @@ r = praw.Reddit(user,user_agent="CaptainScarletBots")
 
 subreddit = r.subreddit("CaptainScarletMemes")
 
-for i in subreddit.new():
+for i in subreddit.new(limit=20):
     if len(i.comments.list()) == 0:
         comment = comment.replace("{{USER}}","/u/"+i.author.name)
         print(r.user.me(), comment)
